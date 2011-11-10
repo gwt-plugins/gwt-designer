f2321a94988c2d4de897df1a90748a922d8dbba5
scheglov <scheglov@google.com>
2011-11-10 14:22:10 +0000

    GWT. Replace copyright headers.

--- com.google.gdt.eclipse.designer.GWTExt/resources/createPublicResources.bat
+++ com.google.gdt.eclipse.designer.GWTExt/resources/createPublicResources.bat
@@ -1,11 +1,10 @@
 @rem ***************************************************************************
 @rem Copyright 2011 Google Inc. All Rights Reserved.
 @rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem     http://www.apache.org/licenses/LICENSE-2.0
+@rem All rights reserved. This program and the accompanying materials
+@rem are made available under the terms of the Eclipse Public License v1.0
+@rem which accompanies this distribution, and is available at
+@rem http://www.eclipse.org/legal/epl-v10.html
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
--- com.google.gdt.eclipse.designer.UiBinder/src/com/google/gdt/eclipse/designer/uibinder/editor/UiBinderReportEntriesProvider.java
+++ com.google.gdt.eclipse.designer.UiBinder/src/com/google/gdt/eclipse/designer/uibinder/editor/UiBinderReportEntriesProvider.java
@@ -1,13 +1,17 @@
-/*
- * Copyright (c) 2011 Google, Inc.
+/*******************************************************************************
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Google, Inc. - initial API and implementation
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.google.gdt.eclipse.designer.uibinder.editor;
 
 import com.google.gdt.eclipse.designer.uibinder.parser.UiBinderContext;
